5f71ff625ae9dca08fe9710c545919f67aac682e
mahdi <mahdi5758hajizadh@gmail.com>
2025-10-31 17:37:12 +0330

    add temp.py

--- app.py
+++
@@ -1,4 +0,0 @@
-x = input("write x : ")
-x = int(x)
-y = x * 4
-print(f"x={x} y={y}")
---
+++ university.py
@@ -0,0 +1,27 @@
+#تکلیف اول
+x=9
+y=x/2
+#z=z+y+2 
+#print(x,y,z)
+
+#تکلیف دوم
+a=3
+b=a+1
+c=a*4/2
+print(a,b,c)
+
+#تکلیف سوم
+age1=int(input("Please enter your age:"))
+age2=int(input("Please enter your age:"))
+age3=int(input("Please enter your age:"))
+age4=int(input("Please enter your age:"))
+plural=(age1+age2+age3+age4)/4
+print(plural)
+
+#تکلیف چهارم
+score1=float(input("Please enter the grade for course1:"))
+unit1=int(input("Enter the units for corse1:"))
+score2=float(input("Please enter the grade for course2:"))
+unit2=int(input("Enter the units for corse2:"))
+plural=(score1*unit1+score2*unit2)/(unit1+unit2)
+print(plural)
